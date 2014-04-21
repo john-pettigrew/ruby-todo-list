473ae7ed5f0fa1784b23384a906d8d64f6fdddda
John Pettigrew <hotfootball2000@gmail.com>
2014-04-21 17:09:31 -0500

    Create basic todo program.

---
+++ todo.rb
@@ -0,0 +1,69 @@
+=begin
+	Simple todo list program written in Ruby
+	by John Pettigrew.
+=end
+
+class Todo_List
+	def initialize()
+		@list =  []
+	end
+	def add(title, completed, due)
+		puts "	(added "+ title + ")"
+		@list << Todo_List_Item.new(title, completed, due)
+	end
+	def get_list()
+		return @list
+	end
+	def print_list
+		@list.each do |item|
+			if item.get_completed == false
+				puts item.get_title + " - " + item.get_due_date + "\n"
+			end
+		end
+	end
+	def complete(item)
+		puts "	(Completed "+item+")"
+		for i in (0 ... @list.length)
+			if item == (@list.at(i)).get_title
+				@list.at(i).complete
+			end
+		end
+	end
+end
+
+class Todo_List_Item
+	def initialize(title, completed, due)
+		@title = title
+		@title.capitalize!
+		@completed = completed
+		@due = due
+	end
+	def complete()
+		@completed = true
+	end
+	def get_title
+		return @title
+	end
+	def get_completed
+		return @completed
+	end
+	def get_due_date
+		return @due
+	end
+
+end
+
+#Testing - creation
+list = Todo_List.new
+list.add("Program", false, "Today")
+list.add("Workout", false, "Today")
+list.add("Swim", false, "Today")
+
+#Testing printing
+puts "\nPrinting Test"
+list.print_list
+
+#testing completion
+puts "\nCompletion Test"
+list.complete("Program")
+list.print_list
